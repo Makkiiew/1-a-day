1c08a21bd8ab58f4ea3cdcb0133814da9042c130
makkiiew <chisholmmmm@gmail.com>
2025-11-27 16:11:23 +0900

    Age Greeter

---
+++ age_greeter.py
@@ -0,0 +1,5 @@
+username = input('Please enter you name ')
+birth_year = input('Please enter your birth year ')
+current_year = 2025
+user_age = (current_year - int(birth_year))
+print(f'Hello {username}, you are {user_age} years old today.')
