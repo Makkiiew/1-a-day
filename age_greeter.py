9e8baaa9b0a2990e34646b5a8d7ee71de46067c8
makkiiew <chisholmmmm@gmail.com>
2025-11-28 15:07:29 +0900

    Ascii

--- age_greeter.py
+++ age_greeter.py
@@ -1,5 +1,10 @@
-username = input('Please enter you name ')
-birth_year = input('Please enter your birth year ')
+#Typo Correction from you
+username = input('Please enter your name ')
+#Updated function that applies int to the input to only allow numbers to be executed
+#birth_year = input('Please enter your birth year ')
+birth_year = int(input('Please enter your birth year '))
 current_year = 2025
-user_age = (current_year - int(birth_year))
+#Calculate age based on current year
+#birth year been cleaned up removing the int data type
+user_age = (current_year - birth_year)
 print(f'Hello {username}, you are {user_age} years old today.')
